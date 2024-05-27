78a159cfa716725822c44be9e0feea0b6565fe36
Bruka25 <bkteshome862@gmail.com>
2024-05-28 02:16:37 +0300

    second task for preventing hoisting with const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
+  /* conditional block */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* prevent hoisting with const */
 
   return [task, task2];
 }
