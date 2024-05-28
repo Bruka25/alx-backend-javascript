896fd9edb09b34477a9ead84f52b03e183449f1f
Bruka25 <bkteshome862@gmail.com>
2024-05-28 03:31:25 +0300

    a function that returns every employee name separated by |

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  let current;
+
+  while ((current = reportWithIterator.next()).done === false) {
+    result += current.value + ' | ';
+  }
+
+  if (result) {
+    result = result.slice(0, -2);
+  }
+
+  return result;
+}
