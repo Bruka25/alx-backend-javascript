dcf4e7318e38b5f0e08a715a6f383f1620f30cc9
Bruka25 <bkteshome862@gmail.com>
2024-05-28 11:52:48 +0300

    update the repo for running the test with jest

---
+++ 0x00-ES6_basic/test.js
@@ -0,0 +1,10 @@
+// 0-constants.test.js
+import { taskFirst, taskNext } from "./0-constants.js";
+
+test("taskFirst returns the first task", () => {
+  expect(taskFirst()).toBe("I prefer const when I can.");
+});
+
+test("taskNext returns the next task", () => {
+  expect(taskNext()).toBe("But sometimes let is okay");
+});
