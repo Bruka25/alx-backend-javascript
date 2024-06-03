c42d23c7eed608aaa9c3a17872f59481ff67a98a
Bruka25 <bkteshome862@gmial.com>
2024-06-03 18:43:59 +0000

    update the third task for requirements

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,8 @@
 function getStudentIdsSum(list) {
+  if (list instanceof Array) {
     return list.reduce((sum, student) => sum + student.id, 0);
+  }
+  return 0;
 }
 
 export default getStudentIdsSum;
