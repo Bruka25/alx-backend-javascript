61511c6e4f94752e45a1ef1b474e0b08091051c0
Bruka25 <bkteshome862@gmial.com>
2024-06-03 20:19:45 +0000

    function to update the grades of students using filter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,13 @@
 function updateStudentGradeByCity(list, city, newGrades) {
-    return list
-        .filter(student => student.location === city)
-        .map(student => {
-            const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-            return {
-                ...student,
-                grade: gradeObj ? gradeObj.grade : 'N/A'
-            };
-        });
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
 }
 
 export default updateStudentGradeByCity;
