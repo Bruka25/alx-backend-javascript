9edd5dc1bec5b2ea24e0df618fc8daac5cc7f1ea
Bruka25 <bkteshome862@gmial.com>
2024-06-03 23:00:57 +0000

    a function that returns an updated map for all the items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (let [key, value] of map.entries()) {
+  for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
     }
