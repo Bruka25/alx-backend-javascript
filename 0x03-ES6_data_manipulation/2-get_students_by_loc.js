3150e057cf5211eb628bbbf6a79cca81831a0b19
Bruka25 <bkteshome862@gmial.com>
2024-06-03 18:51:30 +0000

    update the second task for the requirement

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,8 @@
 function getStudentsByLocation(list, city) {
-	if (list instanceof Array) {
-  return list.filter((student) => student.location === city);
-              }
-	return 0;
+  if (list instanceof Array) {
+    return list.filter((student) => student.location === city);
+  }
+  return 0;
 }
 
 export default getStudentsByLocation;
