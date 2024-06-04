6aab78a6393ac3f8cc62234df043253dd2a8a6b2
Bruka25 <bkteshome862@gmial.com>
2024-06-04 17:58:28 +0000

    a function that an endpoint argument

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+// Export a constant instance of WeakMap named weakMap
+export const weakMap = new WeakMap();
+
+// Export a function named queryAPI
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const currentCount = weakMap.get(endpoint);
+  if (currentCount >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, currentCount + 1);
+}
